688e85cc8345fd09b9bc4731e241cc72e340c749
vefilipo@starbucks.com <vefilipo@starbucks.com>
2023-03-17 16:48:44 -0700

    Added serializer, deserializer and unit tests

---
+++ src/Ranequ.Messaging.Model/MessageState.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Ranequ.Messaging.Model
+{ 
+    public class SerializationService
+    {
+        // Serialize an object to JSON using Newtonsoft.Json
+        public string SerializeToJson<T>(T obj)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                Converters = new[] { new StringEnumConverter() }
+            };
+
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+
+        /// <summary>
+        ///     Deserialize an object from JSON using Newtonsoft.Json
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public T DeserializeFromJson<T>(string json)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                Converters = new[]
+                {
+                    new StringEnumConverter()
+
+                }
+            };
+
+            return JsonConvert.DeserializeObject<T>(json, settings);
+        }
+
+        /// <summary>
+        ///     Serialize an object to XML using System.Xml.Serialization
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public string SerializeToXml<T>(T obj)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+
+            using (var stringWriter = new StringWriter())
+            {
+                serializer.Serialize(stringWriter, obj);
+                return stringWriter.ToString();
+            }
+        }
+
+        /// <summary>
+        ///     Deserialize an object from XML using System.Xml.Serialization
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public T DeserializeFromXml<T>(string xml)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+
+            using (var stringReader = new StringReader(xml))
+            {
+                return (T)serializer.Deserialize(stringReader);
+            }
+        }
+    }
+
+}
---
+++ test/Ranequ.Messaging.Model.Tests/SerializationServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+namespace Ranequ.Messaging.Model.Tests
+{
+    [TestFixture]
+    public class SerializationServiceTests
+    {
+        private SerializationService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new SerializationService();
+        }
+
+        [Test]
+        public void SerializeToJson_SerializesObjectToJson()
+        {
+            var obj = new { Name = "John", Age = 42 };
+            var expectedJson = "{\"Name\":\"John\",\"Age\":42}";
+            var actualJson = _service.SerializeToJson(obj);
+            
+            Assert.AreEqual(expectedJson, actualJson);
+        }
+
+        [Test]
+        public void DeserializeFromJson_DeserializesObjectFromJson()
+        {
+            var json = "{\"Name\":\"John\",\"Age\":42}";
+            var expectedObj = new { Name = "John", Age = 42 };
+            var actualObj = _service.DeserializeFromJson<dynamic>(json);
+
+            Assert.AreEqual(expectedObj.Name, actualObj.Name);
+            Assert.AreEqual(expectedObj.Age, actualObj.Age);
+        }
+
+        [Test]
+        public void SerializeToXml_SerializesObjectToXml()
+        {
+            var obj = new { Name = "John", Age = 42 };
+            var expectedXml = "<root><Name>John</Name><Age>42</Age></root>";
+            var actualXml = _service.SerializeToXml(obj);
+
+            Assert.AreEqual(expectedXml, actualXml);
+        }
+
+        [Test]
+        public void DeserializeFromXml_DeserializesObjectFromXml()
+        {
+            var xml = "<root><Name>John</Name><Age>42</Age></root>";
+            var expectedObj = new { Name = "John", Age = 42 };
+            var actualObj = _service.DeserializeFromXml<dynamic>(xml);
+
+            Assert.AreEqual(expectedObj.Name, actualObj.Name);
+            Assert.AreEqual(expectedObj.Age, actualObj.Age);
+        }
+
+        [Test]
+        public void DeserializeFromXml_DeserializesNestedObjectFromXml()
+        {
+            var xml = "<root><Name>John</Name><Age>42</Age><Address><Street>Main St.</Street><City>Anytown</City></Address></root>";
+            var expectedObj = new { Name = "John", Age = 42, Address = new { Street = "Main St.", City = "Anytown" } };
+            var actualObj = _service.DeserializeFromXml<dynamic>(xml);
+
+            Assert.AreEqual(expectedObj.Name, actualObj.Name);
+            Assert.AreEqual(expectedObj.Age, actualObj.Age);
+            Assert.AreEqual(expectedObj.Address.Street, actualObj.Address.Street);
+            Assert.AreEqual(expectedObj.Address.City, actualObj.Address.City);
+        }
+
+        [Test]
+        public void DeserializeFromXml_DeserializesArrayFromXml()
+        {
+            var xml = "<root><Items><Item>1</Item><Item>2</Item><Item>3</Item></Items></root>";
+            var expectedObj = new int[] { 1, 2, 3 };
+
+            var actualObj = _service.DeserializeFromXml<int[]>(xml);
+
+            Assert.AreEqual(expectedObj.Length, actualObj.Length);
+            for (int i = 0; i < expectedObj.Length; i++)
+            {
+                Assert.AreEqual(expectedObj[i], actualObj[i]);
+            }
+        }
+    }
+}
